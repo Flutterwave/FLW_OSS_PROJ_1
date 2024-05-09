8d9a30f1bf604c59333ac448c995b707167adb33
ChijiokeFLW <chijioke.okorji@flutterwavego.com>
2024-05-09 21:05:21 +0100

    Generated project boilerplate

--- src/app/(pages)/(authenticated)/dashboard/loading.tsx
+++
@@ -1,5 +0,0 @@
-import DashboardSkeleton from '@/ui/base-components/skeletons';
-
-export default function Loading() {
-  return <DashboardSkeleton />;
-}
--- src/app/(pages)/(authenticated)/wallet/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Wallet',
-};
-
-export default function Page() {
-  return <p>Wallet Page</p>;
-}
